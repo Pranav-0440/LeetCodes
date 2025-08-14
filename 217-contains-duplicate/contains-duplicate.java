b4a08a136ea34c3abf61f5004ea97cd063497c83
Pranav Ghorpade <pranavghorpade61@gmail.com>
2025-08-14 18:00:21 +0530

    Time: 19 ms (36.47%) | Memory: 58 MB (78.45%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(containsDuplicate(nums));
+    }
+}
