0657e33b8210427f851e8d3877abdd70241fcc4a
Pranav Ghorpade <pranavghorpade61@gmail.com>
2025-08-14 17:48:41 +0530

    Time: 1 ms (99.54%) | Memory: 57.1 MB (56.13%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxCurrent = nums[0];
+        int maxGlobal = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+            if (maxCurrent > maxGlobal) {
+                maxGlobal = maxCurrent;
+            }
+        }
+        
+        return maxGlobal;
+    }
+    public void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.print(maxSubArray(nums));
+    }
+}
