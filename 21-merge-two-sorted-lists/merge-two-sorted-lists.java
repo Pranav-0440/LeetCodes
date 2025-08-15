ecf131fd2d6d3126a03c9e5191a258d412f921ef
Pranav Ghorpade <pranavghorpade61@gmail.com>
2025-08-15 21:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (8.15%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a dummy node to build the merged list on
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        // While both lists are not empty
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // At this point, at least one list is null
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next; // Return the merged list starting at dummy.next
+    }
+}
